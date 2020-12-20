b39f28fdf70a2c4a21006da883ba6d98a743e8ec
Ben <wolfnun011@gmail.com>
2020-12-20 22:30:06 +0900

    practice: dpulse

---
+++ practice_dpulse_track.rb
@@ -0,0 +1,37 @@
+use_synth :dpulse
+3.times do
+  play chord( :D5, :major)
+  sleep 0.25
+  play chord( :D5, :major)
+  sleep 0.5
+  play chord( :D5, :major)
+  sleep 0.25
+  
+  play chord( :A, :major )
+  sleep 0.5
+  play chord( :A, :major )
+  sleep 0.5
+  
+  play chord( :G, :major )
+  sleep 0.5
+  
+  play chord( :G, :major )
+  sleep 0.5
+  
+  play chord( :A, :major )
+  sleep 1
+end
+
+
+8.times do
+  play chord( :G, :major )
+  sleep 0.25
+end
+
+8.times do
+  play chord( :A, :major )
+  sleep 0.25
+end
+
+play chord( :D5, :major)
+
